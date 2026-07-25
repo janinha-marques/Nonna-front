e36d99ee488aed68bbbc331f0802228163de992c
jane62325526 <janainhamarques98@gmail.com>
2026-07-24 21:40:34 -0300

    atualjs func.

---
+++ public/style/funcoes.js
@@ -0,0 +1,17 @@
+let nomeRestaurante = ("Cantina da Nonna")
+console.log (nomeRestaurante)
+function apresentarPrato (nome, preco,) {
+    console.log("Prato: " + nome + " - R$" + preco)
+}
+
+apresentarPrato ("Pizza Calabresa", 80)
+apresentarPrato ("Raviolli", 45)
+apresentarPrato ("Ravioli, 35")
+apresentarPrato ("Margherita, 60")
+
+
+
+
+
+
+
